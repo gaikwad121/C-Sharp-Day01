d3ab85c5fa5c79f28659927becfa04d9ded44729
gaikwad121 <jg.jyotigaikwad121@gmail.com>
2025-03-18 01:21:15 +0530

    Added new programs

---
+++ C#Pattern/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace project;
+class Pattern
+{
+    static void Main()
+    {
+        pattern(4);
+        squarePattern(4);
+        alphabetpattern(4);
+    }
+    /* /3.
+     * /*
+  * A B C D
+  * A B C D
+  * A B C D
+  * A B C D
+  * 
+  */
+    static void alphabetpattern(int n)
+    {
+        
+        for(int i = 1; i <= n; i++)
+        {
+            char ch = 'A';
+            for (int j = 1; j <= n; j++)
+            {
+                Console.Write((char)ch);
+                ch++;
+            }
+            Console.WriteLine();
+        }
+    }
+    /*/2.
+****
+****
+****
+****
+*/
+    static void squarePattern(int n)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+        for(int j = 1; j <= n; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+
+    /*
+     * 
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4*/
+    static void pattern(int n)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+        for(int j = 1; j <= n; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+
+    }
+}
+
