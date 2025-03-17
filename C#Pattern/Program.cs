e3ef1278f8b7b4350493f42fe2b51ccade363b67
gaikwad121 <jg.jyotigaikwad121@gmail.com>
2025-03-18 02:08:30 +0530

    added methods

--- C#Pattern/Program.cs
+++ C#Pattern/Program.cs
@@ -1,72 +1,206 @@
 ﻿using System;
-namespace project;
-class Pattern
+using System.ComponentModel;
+//pattern2(4);
+//squarePattern(4);
+alphabetpattern(4);
+//NoPattern(3);
+starPattern(4);
+pattern(4);
+noPattern(4);
+floyds(4);
+pattern1(4);
+pattern5(4);
+
+
+/*     1
+    1 2 1
+  1 2 3 2 1
+1 2 3 4 3 2 1*/
+static void pattern5(int n)
 {
-    static void Main()
+    for(int i = 1; i <= n; i++)
     {
-        pattern(4);
-        squarePattern(4);
-        alphabetpattern(4);
-    }
-    /* /3.
-     * /*
-  * A B C D
-  * A B C D
-  * A B C D
-  * A B C D
-  * 
-  */
-    static void alphabetpattern(int n)
-    {
-        
-        for(int i = 1; i <= n; i++)
+    for(int j  = 1; j <= n - i; j++)
         {
-            char ch = 'A';
-            for (int j = 1; j <= n; j++)
-            {
-                Console.Write((char)ch);
-                ch++;
-            }
-            Console.WriteLine();
+            Console.Write(" ");
         }
+for(int j = 1; j <= i; j++)
+        {
+            Console.Write(j);
+        }
+for(int j = i - 1; j >= 1; j--)
+        {
+            Console.Write(j);
+        }
+        Console.WriteLine();
     }
-    /*/2.
+}
+/*1 1 1 1
+    2 2 2
+       3 3
+         4
+*/
+static void pattern1(int n)
+{
+for(int i = 0; i <n; i++)
+    {
+for(int j = 0; j <i; j++)
+        {
+            Console.Write(" ");
+        }
+for(int j = 0; j <n - i; j++)
+        {
+            Console.Write(i+1);
+        }
+        Console.WriteLine();
+    }
+}
+/*Floyd's Triangle pattern
+1
+2 3
+4 5 6
+7 8 9 10 */
+static void floyds(int n)
+{
+    int count = 1;
+for(int i = 1; i <= n; i++)
+    {
+for(int j = 1; j <= i; j++)
+        {
+            Console.Write(count+" ");
+            count++;
+        }
+        Console.WriteLine();
+    }
+
+}
+/*
+1
+1 2
+1 2 3
+1 2 3 4*/
+static void noPattern(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+    for(int j = 1; j <= i; j++)
+        {
+            Console.Write(j + " ");
+        }
+        Console.WriteLine();
+    }
+}
+/*
+1
+2 2
+3 3 3
+4 4 4 4*/
+static void pattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+/*
+*
+**
+***
+****
+*/
+void starPattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
+/*
+1 2 3
+4 5 6
+7 8 9*/
+void NoPattern(int n)
+{
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            Console.Write(count + " ");
+            count++;
+        }
+
+
+        Console.WriteLine();
+    }
+}
+
+/*
+* A B C D
+* A B C D
+* A B C D
+* A B C D
+* 
+*/
+void alphabetpattern(int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        char ch = 'A';
+        for (int j = 1; j <= n; j++)
+        {
+            Console.Write((char)ch);
+            ch++;
+        }
+        Console.WriteLine();
+    }
+}
+/*/2.
 ****
 ****
 ****
 ****
 */
-    static void squarePattern(int n)
+void squarePattern(int n)
+{
+    for (int i = 1; i <= n; i++)
     {
-        for(int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
         {
-        for(int j = 1; j <= n; j++)
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine();
+            Console.Write("*");
         }
-        Console.ReadLine();
+        Console.WriteLine();
     }
+    Console.ReadLine();
+}
 
-    /*
-     * 
+/*
+ * 
 1 2 3 4
 1 2 3 4
 1 2 3 4
 1 2 3 4*/
-    static void pattern(int n)
+void pattern2(int n)
+{
+    for (int i = 1; i <= n; i++)
     {
-        for(int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
         {
-        for(int j = 1; j <= n; j++)
-            {
-                Console.Write(j + " ");
-            }
-            Console.WriteLine();
+            Console.Write(j + " ");
         }
-        Console.ReadLine();
-
+        Console.WriteLine();
     }
+    Console.ReadLine();
+
 }
 
--- C#Pattern/obj/Debug/net8.0/C#Pattern.css.AssemblyInfo.cs
+++ C#Pattern/obj/Debug/net8.0/C#Pattern.css.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Pattern.css")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ab85c5fa5c79f28659927becfa04d9ded44729")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Pattern.css")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Pattern.css")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
