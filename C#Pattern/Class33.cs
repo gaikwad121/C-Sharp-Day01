958ef870bcc44f7ce96c98af6bc05fda9689c4ca
Jyoti <jg.jyotigaikwad121@gmail.com>
2025-09-24 23:53:07 +0530

    Add

---
+++ C#Pattern/Class33.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Pattern
+{
+	internal class Class33
+	{
+		public static void Add()
+		{
+			int a = 20;
+			int b = 46;
+			int c = a + b;
+
+			Console.WriteLine(c);
+		}
+	}
+}
